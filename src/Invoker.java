8b0a2ca8f5dba0d6585bce1e4f9d036fe0eca03a
bkhou <m.ibrahim.376f@gmail.com>
2025-08-05 16:32:40 +0800

    edited index for Invoker.java

--- src/Invoker.java
+++ src/Invoker.java
@@ -1,14 +1,14 @@
 import java.util.Stack;
 
 public class Invoker {
-    private Command[] commands;
+    private Command[] cmdToExecute;
 
     public void setCommandsForExecution(Command[] commands) {
-        this.commands = commands;
+        this.cmdToExecute = commands;
     }
     public void executeCommand(Stack <Command> history) {
-        if (commands != null){
-            for (Command commands : commands){
+        if (cmdToExecute != null){
+            for (Command commands : cmdToExecute){
                 commands.execute();
                 if (commands.isStackable()){
                     history.push(commands);
