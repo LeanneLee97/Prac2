3bd137b077eae4ca863d781b237492b9456cbf27
Lee Ying <54098853+LeanneLee97@users.noreply.github.com>
2025-08-06 15:57:47 +0800

    Updated EmailValidator.java
    allow underscores in local part

--- src/EmailValidator.java
+++ src/EmailValidator.java
@@ -1,12 +1,15 @@
 public class EmailValidator {
     public static boolean isValidEmail(String email) {
         String regex =
-            "^[A-Za-z0-9_](?:[A-Za-z0-9_]|(?<![.-])[.-](?![.-]))*[A-Za-z0-9_]@" +  // Local part
-            "(?![.-])[A-Za-z0-9.-]+(?<![.-])\\." +                                 // Domain
-            "[a-z]{2,3}$";                                                         // Extension
+                "^[A-Za-z0-9_]+(?:[.-][A-Za-z0-9_]+)*" +    // local part
+                        "@" +
+                        "(?!.*[_])" +                                // no underscore in domain
+                        "[A-Za-z0-9]+(?:[.-][A-Za-z0-9]+)*" +       // domain
+                        "\\.[a-z]{2,3}$";                            // extension (2-3 lowercase letters)
 
         return email.matches(regex);
     }
+
 
 }
 
