f41b94c1ca3214cf014bd867b8790d1424750fb8
Ibrahim <m.ibrahim.376f@gmail.com>
2025-08-06 12:43:32 +0800

    Update UpdateCommand.java

--- src/UpdateCommand.java
+++ src/UpdateCommand.java
@@ -1,42 +1,46 @@
-//package com.yourdomain.employeemgr;
-
-import java.util.List;
-
 public class UpdateCommand implements Command {
-    private final Receiver receiver;
-    private final int index;
-    private final Employee newEmployee;
-    private Employee previousEmployee;
+    private final Receiver taskList;
+    private int index;
+    private String data1;
+    private String data2;
+    private String data3;
+    private String previousTask;
+    private final String payload;
 
-    public UpdateCommand(Receiver receiver, int index, Employee newEmployee) {
-        this.receiver = receiver;
-        this.index = index;
-        this.newEmployee = newEmployee;
+    public UpdateCommand(Receiver taskList, String payload) {
+        this.taskList = taskList;
+        this.payload = payload;
     }
 
     @Override
-    public boolean execute() {
-        if (index < 1 || index > receiver.getEmployees().size()) {
-            System.out.println("Invalid index.");
-            return false;
+    public void execute() {
+        String[] splitPayload = payload.split(" ");
+        if(splitPayload.length != 4){
+            throw new IllegalArgumentException("Invalid payload: Expecting 4 " +
+                    "data fields");
         }
+        try{
+            index = Integer.parseInt(splitPayload[0]) - 1;
+        }catch(NumberFormatException e){
+            throw new IllegalArgumentException("Invalid index");
+        }
+        data1 = splitPayload[1];
+        data2 = splitPayload[2];
+        data3 = splitPayload[3];
 
-        previousEmployee = receiver.getEmployees().get(index - 1);
-        receiver.update(index, newEmployee);
-        return true;
+        //taskList.validateTask(data3);
+        previousTask = taskList.getTask(index);
+        String updatedTask = String.join(" ", data1, data2, data3);
+        taskList.updateTask(index, updatedTask);
     }
 
     @Override
     public void undo() {
-        if (previousEmployee != null) {
-            receiver.update(index, previousEmployee);
-        } else {
-            System.out.println("No previous state to revert to.");
-        }
+        taskList.updateTask(index, previousTask);
     }
 
     @Override
-    public boolean isUndoable() {
+    public boolean isStackable() {
         return true;
     }
 }
