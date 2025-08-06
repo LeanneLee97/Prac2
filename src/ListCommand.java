db882977c3176772ce35c8f32989dc20961ec1c4
Ibrahim <m.ibrahim.376f@gmail.com>
2025-08-06 12:45:00 +0800

    Update ListCommand.java

--- src/ListCommand.java
+++ src/ListCommand.java
@@ -1,23 +1,33 @@
+import java.util.ArrayList;
+
 public class ListCommand implements Command {
-    private final Receiver receiver;
+    private final Receiver taskList;
 
-    public ListCommand(Receiver receiver) {
-        this.receiver = receiver;
+    public ListCommand(Receiver tasklist) {
+        this.taskList = tasklist;
     }
 
     @Override
-    public boolean execute() {
-        receiver.listAll();
-        return true;
+    public void execute(){
+        ArrayList<String> list = taskList.getAllTasks();
+
+        if (list.isEmpty()) {
+            System.out.println("No tasks available.");
+        } else {
+            for (int i = 0; i < list.size(); i++) {
+                System.out.println((i+1) + ": " + list.get(i));
+            }
+        }
+        taskList.getAllTasks();
     }
 
     @Override
-    public void undo() {
-        // Listing doesn't modify state, so nothing to undo
+    public void undo(){
+
     }
 
     @Override
-    public boolean isUndoable() {
+    public boolean isStackable() {
         return false;
     }
 }
