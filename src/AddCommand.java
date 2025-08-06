61f3ab23cdc5e81ef1073491edd58b826847b9b4
Ibrahim <m.ibrahim.376f@gmail.com>
2025-08-06 15:27:06 +0800

    Update AddCommand.java

--- src/AddCommand.java
+++ src/AddCommand.java
@@ -22,7 +22,7 @@ public class AddCommand implements Command {
         data2 = splitPayload[1];
         data3 = splitPayload[2];
 
-        if (!isValidEmail(data3)){
+        if (!EmailValidator.isValidEmail(data3)) {
             throw new CustomException("Invalid email");
         }
 
