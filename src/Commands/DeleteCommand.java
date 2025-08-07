4b79ee5140e97bdabc56185462593fe74dc0b7e9
Ibrahim <m.ibrahim.376f@gmail.com>
2025-08-07 09:08:12 +0800

    Update DeleteCommand.java
    overload add method

--- src/Commands/DeleteCommand.java
+++ src/Commands/DeleteCommand.java
@@ -22,7 +22,7 @@ public class DeleteCommand implements Command {
     @Override
     public void undo() {
         if (removedTask != null) {
-            taskList.reAddTask(index, removedTask);
+            taskList.addTask(index, removedTask);
         }
     }
 
@@ -32,3 +32,4 @@ public class DeleteCommand implements Command {
     }
 
 }
+
