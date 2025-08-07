2e6352b68031fd369c13fcc31a2c456292441288
bkhou <m.ibrahim.376f@gmail.com>
2025-08-07 14:15:40 +0800

    SentenceCase editor for AddCommand.java and UpdateCommand.java

--- src/Validator/EmailValidator.java
+++ src/Validator/EmailValidator.java
@@ -5,15 +5,15 @@ public class EmailValidator {
         if (email.equals("Email")) {
             return true;
         } // placeholder for now, we need a checking logic to look for "@" in the email vs word
-        else{
-            String regex =
-                    "(^[A-Za-z0-9_]+(?:[.-][A-Za-z0-9_]+)*" +    // local part
-                            "@" +
-                            "(?!.*[_])" +                                // no underscore in domain
-                            "[A-Za-z0-9]+(?:[.-][A-Za-z0-9]+)*" +       // domain
-                            "\\.[a-z]{2,3}$)";
-            return email.matches(regex);//
-        }
+
+        String regex =
+                "(^[A-Za-z0-9_]+(?:[.-][A-Za-z0-9_]+)*" +    // local part
+                        "@" +
+                        "(?!.*[_])" +                                // no underscore in domain
+                        "[A-Za-z0-9]+(?:[.-][A-Za-z0-9]+)*" +       // domain
+                        "\\.[a-z]{2,3}$)";
+        return email.matches(regex);//
+
         // extension (2-3 lowercase letters)
     }
 }
