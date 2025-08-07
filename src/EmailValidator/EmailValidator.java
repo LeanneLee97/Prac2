af4ded7f683d42e4095991f91eab4ea8d3169e97
bkhou <m.ibrahim.376f@gmail.com>
2025-08-07 11:26:21 +0800

    Edited Email| in line 6 of EmailValidator.java

--- src/EmailValidator/EmailValidator.java
+++ src/EmailValidator/EmailValidator.java
@@ -3,11 +3,12 @@ package EmailValidator;
 public class EmailValidator {
     public static boolean isValidEmail(String email) {
         String regex =
-                "^[A-Za-z0-9_]+(?:[.-][A-Za-z0-9_]+)*" +    // local part
+                "(Email|^[A-Za-z0-9_]+(?:[.-][A-Za-z0-9_]+)*" +    // local part
                         "@" +
                         "(?!.*[_])" +                                // no underscore in domain
                         "[A-Za-z0-9]+(?:[.-][A-Za-z0-9]+)*" +       // domain
-                        "\\.[a-z]{2,3}$";                            // extension (2-3 lowercase letters)
+                        "\\.[a-z]{2,3}$)";                            //
+        // extension (2-3 lowercase letters)
 
         return email.matches(regex);
     }
