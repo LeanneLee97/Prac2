178f5b739c4266bfb33651b45df674901a333317
Lee Ying <54098853+LeanneLee97@users.noreply.github.com>
2025-08-07 13:58:39 +0800

    Update EmailValidator.java

--- src/Validator/EmailValidator.java
+++ src/Validator/EmailValidator.java
@@ -2,6 +2,9 @@ package Validator;
 
 public class EmailValidator {
     public static boolean isValidEmail(String email) {
+        if (email.equals("Email")) {
+            return true;
+        } // placeholder for now, we need a filtering logic for "@" vs word
         String regex =
                 "(^[A-Za-z0-9_]+(?:[.-][A-Za-z0-9_]+)*" +    // local part
                         "@" +
@@ -12,7 +15,5 @@ public class EmailValidator {
 
         return email.matches(regex);
     }
-
-
 }
 
