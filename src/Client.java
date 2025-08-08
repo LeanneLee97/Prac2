b8767b35f038205413702bdf5632acb61edce711
bkhou <m.ibrahim.376f@gmail.com>
2025-08-08 10:39:12 +0800

    allow ability to input add, Add, ADd, aDD, ADD to cater for typos for all commands

--- src/Client.java
+++ src/Client.java
@@ -12,41 +12,48 @@ public class Client {
     Invoker invoker = new Invoker();
 
     String[] inputs = {
-            "add First_name Last_name Email",
-            "add ^john Doe simple@example.com",
-            "add hanna moon tetter.tots@potatoesarelife.com",
-            "add Ah Boon Green-tea@teaforlife.com",
+            "Add First_name Last_name Email",
+            "add john Doe simple@example.com",
+            "aDd Hanna moon tetter.tots@potatoesarelife.com",
+            "ADD Ah Boon Green-tea@teaforlife.com",
             "list",
+            "update 3 Adam Sun Adamisbest@email.com",
+            "list",
+            "undo",
+            "update 3 Adam Sun",
+            "list",
+            "undo",
             "update 3 Adam",
             "list",
             "update 1 Blue Bell ice-cream@alaskaFields.org",
             "list",
-            "delete 1",
+            "Delete 1",
             "list",
             "undo",
             "list",
             "delete 1",
             "list",
             "delete 7",
-            "update 12 Steven"
+            "update 12 Steven",
+            "hello",
     };
 
     for (String input : inputs) {
-        //System.out.println(input);
         Command command = null;
+        String lowerCase = input.toLowerCase();
 
-        if (input.startsWith("add ")) {
+        if (lowerCase.startsWith("add")) {
             String payload = input.substring(4).trim();
             command = new AddCommand(receiver, payload);
-        }  if (input.startsWith("update ")) {
+        }  if (lowerCase.startsWith("update")) {
             String payload = input.substring(7).trim();
             command = new UpdateCommand(receiver, payload);
-        }  if (input.startsWith("delete ")) {
+        }  if (lowerCase.startsWith("delete")) {
             String payload = input.substring(7).trim();
             command = new DeleteCommand(receiver, payload);
-        }  if (input.equalsIgnoreCase("list")) {
+        }  if (lowerCase.equalsIgnoreCase("list")) {
             command = new ListCommand(receiver);
-        }  if (input.equalsIgnoreCase("undo")) {
+        }  if (lowerCase.equalsIgnoreCase("undo")) {
             command = new UndoCommand(receiver, history);
         }
 
