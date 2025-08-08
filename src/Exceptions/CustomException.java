81c77fb845ed29c036bf5899eb15a377c39270f2
Lee Ying <54098853+LeanneLee97@users.noreply.github.com>
2025-08-08 16:57:21 +0800

    Update CustomException.java
    add javaDocs

--- src/Exceptions/CustomException.java
+++ src/Exceptions/CustomException.java
@@ -1,6 +1,18 @@
 package Exceptions;
+/**
+ * A custom exception to help narrow down application specific errors.
+ * This exception can be thrown in cases where a specific error message
+ * needs to be conveyed to users, since scenarios with built-in
+ * exceptions are too generic.
+ *
+ */
 public class CustomException extends Exception {
+    /**
+     * Constructs a new CustomException with the specified detail message.
+     *
+     */
     public CustomException(String message) {
         super(message);
     }
 }
+
