5e1b49b3e9e06c7b4dcf41c8443ff56406f02b70
Ibrahim <m.ibrahim.376f@gmail.com>
2025-08-08 17:05:01 +0800

    Update AddCommand.java
    Added javadocs

--- src/Commands/AddCommand.java
+++ src/Commands/AddCommand.java
@@ -3,6 +3,13 @@ import Receiver.Receiver;
 import Exceptions.CustomException;
 import Validator.*;
 
+/**
+ * Class used to add new entries into taskList. This command expects a
+ * payload consisting of <data1>, <data2> and <data3></>. <data1> and
+ * <data2></> should be formatted to be Titlecase. <data3></> only able to
+ * accept a single email address or Latin letters (case-insensitive), digits 0 to 9 and
+ * underscores.
+ */
 public class AddCommand implements Command {
     private final Receiver taskList;
     private String data1;
@@ -10,14 +17,29 @@ public class AddCommand implements Command {
     private String data3;
     private final String payload;
 
+    /**
+     * Constructs an {@code AddCommand} with the specified receiver and input payload.
+     *
+     * @param taskList the receiver that manages the task list
+     * @param payload the input string containing three fields separated by whitespace
+     */
     public AddCommand(Receiver taskList, String payload) {
         this.taskList = taskList;
         this.payload = payload;
     }
 
+    /**
+     * @code execute Executes the add command by validating and formatting the
+     * input,
+     * then adding the resulting task to the receiver's task list.
+     *
+     * @throws CustomException if the payload is invalid or fails validation
+     */
     @Override
     public void execute() throws CustomException{
+        // Split the payload into the 3 data fields
         String[] splitPayload = payload.split("\\s+");
+        //Check if there are exactly 3 data fields
         if (splitPayload.length != 3) {
             throw new CustomException("Invalid payload: Expecting 3 " +
                     "data fields");
@@ -37,22 +59,25 @@ public class AddCommand implements Command {
                 throw new CustomException("Invalid word.");
             }
         }
-
-
-
-            //taskList.validateTask(data3);
-
             String task = String.join(" ", data1, data2, data3);
             taskList.addTask(task);
             System.out.println("add");
     }
 
+    /**
+     * Undoes the add command by removing the previously added task from the receiver.
+     */
     @Override
     public void undo() {
         String task = String.join(" ", data1, data2, data3);
         taskList.deleteTask(task);
         System.out.println("Undo");
     }
+    /**
+     * Indicates whether this command should be stored in the undo history.
+     *
+     * @return {@code true} since add commands are stackable
+     */
 
     @Override
     public boolean isStackable() {
